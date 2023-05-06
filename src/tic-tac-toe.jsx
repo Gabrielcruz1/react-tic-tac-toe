65510d0d8a129f7e3dde7807dc9dfa7692f1ad97
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-05-05 22:15:57 -0700

    lift state up, neeeds game memory

--- src/tic-tac-toe.jsx
+++ src/tic-tac-toe.jsx
@@ -13,7 +13,7 @@ function Square({ value, onSquareClick }) {
 
 
 
-export default function Board() {
+function Board() {
     //Taking turns state 
     const [xIsNext, setXIsNext] = useState(true)
     //creates an array with 9 el and sets them to null. 
@@ -70,6 +70,23 @@ export default function Board() {
             </div>
         </>
     )
+}
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)])
+    const currentSquares = history[history.length - 1];
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    );
 }
 
 
